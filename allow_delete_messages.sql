a460a0cc2b5b20f3da6818de6f76e86f407ccae9
qrucio <divyansh832009@gmail.com>
2025-12-12 19:44:39 +0530

    beedi lelo baby retry

--- allow_delete_messages.sql
+++
@@ -1,8 +0,0 @@
--- ENABLE MESSAGE DELETION
--- Policy to allow users to delete their OWN messages.
-
-create policy "Users can delete own messages"
-on public.messages for delete
-using (
-    sender_id = auth.uid()
-);
